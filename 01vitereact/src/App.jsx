7007ffb1c7399019ca1c3463d128b21c0edcab29
Santosh Bohara <sb86981@gmail.com>
2025-07-15 19:51:18 +0545

    basic react done

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Santosh from "./santox";
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Santosh />
+      <h1>This is Santosh here.</h1>
+      <p>test para</p>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/santox.jsx
@@ -0,0 +1,5 @@
+function Santosh() {
+  return <h3>hello</h3>;
+}
+
+export default Santosh;
