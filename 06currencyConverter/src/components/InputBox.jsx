8628e4b4f75e4b0727696b06fa7c7f0eabb30e69
Santosh Bohara <sb86981@gmail.com>
2025-07-19 17:12:06 +0545

    error fixed and complete

--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -15,15 +15,15 @@ function InputBox({
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <lable
+        <label
           htmlFor={amountInputId}
           className="text-black/40 mb-2 inline-block"
         >
           {label}
-        </lable>
+        </label>
         <input
           id={amountInputId}
-          className="outline-none w-full bg-white bg-transparent py-1.5"
+          className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
           disabled={amountDisable}
