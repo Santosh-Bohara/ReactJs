23921c4d68c35fd85896da79e03bb13a13284115
Santosh Bohara <sb86981@gmail.com>
2025-07-19 14:54:50 +0545

    create custome hook in src file.

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl flex-center text-center bg-orange-500">
+        Currency App
+      </h1>
+    </>
+  );
 }
 
 export default App;
