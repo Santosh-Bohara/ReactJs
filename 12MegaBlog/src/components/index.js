6007b819f4e99f4ced7fde0d916b6fe3c6bb7cc4
Santosh Bohara <sb86981@gmail.com>
2025-07-30 20:35:24 +0545

    complete all pages

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -4,5 +4,22 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import AddPost from "../pages/AddPost";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  AddPost,
+  PostForm,
+  PostCard,
+};
---
+++ 12MegaBlog/src/pages/AddPost.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { AddPost as AddPostComponent, Container } from "../components";
+import { Container, PostForm } from "../components";
+
+function Signup() {
+  return (
+    <div className="py-8">
+      <Container>
+        <PostForm />
+      </Container>
+    </div>
+  );
+}
+
+export default AddPost;
